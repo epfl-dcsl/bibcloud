c6c68f3d4ea23b46a684e9bd5eb2d485fc6ec93d
edouardbugnion <edouard.bugnion@epfl.ch>
2018-07-17 14:26:24 +0200

    woot

--- makeabbrev.py
+++ makeabbrev.py
@@ -86,7 +86,8 @@ CONF = {
     "hoti"    : ['y', "Proceedings of the YEAR Annual Symposium on High-Performance Interconnects"],
     "rtas"    : ['y', "Proceedings of the YEAR Real-Time and Embedded Technology and Applications Symposium"],
     'hotcloud'    : ['o', "Proceedings of the OCCURENCE  workshop on Hot topics in Cloud Computing (HotCloud)"],
-    "cloud" : ['o', "Proceedings of the OCCURENCE IEEE International Conference on Cloud Computing (CLOUD)"]
+    "cloud" : ['o', "Proceedings of the OCCURENCE IEEE International Conference on Cloud Computing (CLOUD)"],
+    "woot" : ['o', "Proceedings of the OCCURENCE {USENIX} Workshop on Offensive Technologies (WOOT)"]
 }
 
 SHORTCONF = {
@@ -365,6 +366,7 @@ annual_occ("spaa",12,2000,2018)
 annual_occ("vldb",26,2000,2008)
 annual_occ("sigcse",42,2011,2012)
 annual_occ("cloud",2,2009,2018)
+annual_occ("woot",4,2010,2018)
 F.close()
 
 
