6135e50b4ecbab705562d9d706878df31e5d4b85
edouardbugnion <edouard.bugnion@epfl.ch>
2018-12-07 05:23:51 +0100

    ccgrid

--- makeabbrev.py
+++ makeabbrev.py
@@ -97,7 +97,8 @@ CONF = {
     "simpar" : ['y', "Proceedings of the YEAR IEEE International Conference on Simulation, Modeling, and Programming for Autonomous Robots (SIMPAR)"],
     "nips"   : ['y', "Proceedings of the YEAR Annual Conference on Neural Information Processing Systems (NIPS)"],
     "kbnets@sigcomm" : ['y',"Proceedings of the YEAR Workshop on Kernel-Bypass Networks (KBNETS@SIGCOMM)"],
-    "sigir" : ['o', "Proceedings of the OCCURENCE International ACM SIGIR conference on Research and Development in Information Retrieval (SIGIR)"]
+    "sigir" : ['o', "Proceedings of the OCCURENCE International ACM SIGIR conference on Research and Development in Information Retrieval (SIGIR)"],
+    "ccgrid" : ['o', "Proceedings of the OCCURENCE IEEE/ACM International Symposium on Cluster, Cloud and Grid Computing (CCGRID)"]
 }
 
 SHORTCONF = {
@@ -386,11 +387,13 @@ annual_occ("wren",1,2009,2010)
 annual_year("simpar",2008,2018)
 annual_year("nips",2010,2018)
 annual_year("kbnets@sigcomm",2016,2019)
+annual_occ("sigir",35,2012,2019)
+annual_occ("ccgrid",10,2010,2019)
 
 #crazy ones:
 annual_year("iwmm",1992,1996)
 annual_occ("ismm",4,2004,2005)
-annual_occ("sigir",35,2012,2019)
+
 
 
 for occ in range (1,9):
