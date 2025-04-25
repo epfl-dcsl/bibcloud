1e16553bd81a24ea114dac369e81d076058578d6
Edouard Bugnion <edouard.bugnion@epfl.ch>
2025-04-25 08:03:54 +0200

    hpca - maxyear

--- makeabbrev.py
+++ makeabbrev.py
@@ -353,7 +353,7 @@ for y in range(2016,2018):
     F.write(make_sigcomm("hotmiddlebox@sigcomm",y))
 
 
-annual_occ("hpca",1,1995,2019)
+annual_occ("hpca",1,1995,MAXYEAR)
 annual_occ("micro",1,1968,MAXYEAR)
 
 
